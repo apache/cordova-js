dbc00413e5dac99d3bd5bc8107cfed11ee1f187f
Gord Tanner <gtanner@gmail.com>
2011-11-30 10:07:34 -0500

    added the rest of the tests for accelerometer

--- test/test.accelerometer.js
+++ test/test.accelerometer.js
@@ -52,27 +52,122 @@ describe("accelerometer", function () {
             });
         });
 
-        it("calls exec to get the timeout", function () {
+        describe("when working with the timeout", function () {
+            it("calls exec to get the timeout", function () {
+                var success = jasmine.createSpy(),
+                    fail = jasmine.createSpy();
+
+                spyOn(window, "setInterval");
+                accelerometer.watchAcceleration(success, fail);
+
+                expect(PhoneGap.exec).toHaveBeenCalledWith(jasmine.any(Function), jasmine.any(Function), "Accelerometer", "getTimeout", []);
+            });
+
+            it("it sets it to 10 seconds greater than the frequency", function () {
+                var success = jasmine.createSpy(),
+                    fail = jasmine.createSpy();
+
+                spyOn(window, "setInterval");
+                accelerometer.watchAcceleration(success, fail, {frequency: 5000});
+
+                //execute the success callback ;)
+                PhoneGap.exec.mostRecentCall.args[0](5000);
+
+                expect(PhoneGap.exec).toHaveBeenCalledWith(null, null, "Accelerometer", "setTimeout", [15000]);
+            });
+
+            it("doesn't set it if timeout is less than freq + 10sec", function () {
+                var success = jasmine.createSpy(),
+                    fail = jasmine.createSpy();
+
+                spyOn(window, "setInterval");
+                accelerometer.watchAcceleration(success, fail, {frequency: 1000});
+
+                //execute the success callback ;)
+                PhoneGap.exec.mostRecentCall.args[0](20000);
+
+                expect(PhoneGap.exec).not.toHaveBeenCalledWith(null, null, "Accelerometer", "setTimeout", [11000]);
+            });
+        });
+
+        it("returns a generated interval id", function () {
+            var utils = require('phonegap/utils'),
+                success = jasmine.createSpy(),
+                fail = jasmine.createSpy();
+
+            spyOn(window, "setInterval");
+            spyOn(utils, "createUUID").andReturn(42);
+
+            expect(accelerometer.watchAcceleration(success, fail)).toBe(42);
+        });
+
+        it("returns the interval ID", function () {
+            var utils = require('phonegap/utils'),
+                success = jasmine.createSpy(),
+                fail = jasmine.createSpy();
+
+            spyOn(window, "setInterval").andReturn(1);
+            spyOn(utils, "createUUID").andReturn(42);
+
+            accelerometer.watchAcceleration(success, fail);
+
+            expect(accelerometer.timers[42]).toBe(1);
+        });
+
+        it("starts the interval with the provided frequency", function () {
+            var success = jasmine.createSpy(),
+                fail = jasmine.createSpy();
+
+            spyOn(window, "setInterval");
+            accelerometer.watchAcceleration(success, fail, {frequency: 11});
+
+            expect(window.setInterval).toHaveBeenCalledWith(jasmine.any(Function), 11);
+        });
+
+        it("starts the interval with the default frequency", function () {
             var success = jasmine.createSpy(),
                 fail = jasmine.createSpy();
 
             spyOn(window, "setInterval");
             accelerometer.watchAcceleration(success, fail);
 
-            expect(PhoneGap.exec).toHaveBeenCalledWith(jasmine.any(Function), jasmine.any(Function), "Accelerometer", "getTimeout", []);
+            expect(window.setInterval).toHaveBeenCalledWith(jasmine.any(Function), 10000);
         });
 
-        it("sets the timeout to be greater than 10 seconds if less", function () {
-            var success = jasmine.createSpy(),
+        it("gets the acceleration for the provided interval", function () {
+            var utils = require('phonegap/utils'),
+                success = jasmine.createSpy(),
                 fail = jasmine.createSpy();
 
             spyOn(window, "setInterval");
-            accelerometer.watchAcceleration(success, fail, {frequency: 5000});
+            accelerometer.watchAcceleration(success, fail, {frequency: 11});
 
-            //execute the success callback ;)
-            PhoneGap.exec.mostRecentCall.args[0](5000);
+            //exec the interval callback!
+            window.setInterval.mostRecentCall.args[0]();
 
-            expect(PhoneGap.exec).toHaveBeenCalledWith(null, null, "Accelerometer", "setTimeout", [15000]);
+            expect(PhoneGap.exec).toHaveBeenCalledWith(success, fail, "Accelerometer", "getAcceleration", []);
+        });
+    });
+
+    describe("when clearing the watch", function () {
+        beforeEach(function () {
+            spyOn(window, "clearInterval");
+        });
+
+        it("doesn't clear anything if the timer doesn't exist", function () {
+            accelerometer.clearWatch("Never Gonna Give you Up");
+            expect(window.clearInterval).not.toHaveBeenCalled();
+        });
+
+        it("can be called with no args", function () {
+            accelerometer.clearWatch();
+            expect(window.clearInterval).not.toHaveBeenCalled();
+        });
+
+        it("clears the interval if the id exists", function () {
+            accelerometer.timers["frank"] = "beans";
+            accelerometer.clearWatch("frank");
+            expect(window.clearInterval).toHaveBeenCalledWith("beans");
         });
     });
 });
