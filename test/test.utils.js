d50158c3e1bb65bb2bcde88de9b7a95fb7390c1d
Gord Tanner <gtanner@gmail.com>
2011-12-15 11:00:54 -0500

    added some more utils tests

--- test/test.utils.js
+++ test/test.utils.js
@@ -1,9 +1,62 @@
 describe("utils", function () {
     var utils = require('phonegap/utils');
 
-    describe("clone", function () {
+    describe("when cloning", function () {
+        it("can clone an array", function () {
+            var orig = [1, 2, 3, {four: 4}, "5"];
+
+            expect(utils.clone(orig)).toEqual(orig);
+            expect(utils.clone(orig)).not.toBe(orig);
+        });
+
         it("can clone null", function () {
             expect(utils.clone(null)).toBeNull();
+        });
+
+        it("can clone undefined", function () {
+            expect(utils.clone(undefined)).not.toBeDefined();
+        });
+
+        it("can clone a function", function () {
+            var f = function () { return 4; };
+            expect(utils.clone(f)).toBe(f);
+        });
+
+        it("can clone a number", function () {
+            expect(utils.clone(4)).toBe(4);
+        });
+
+        it("can clone a string", function () {
+            expect(utils.clone("why")).toBe("why");
+        });
+
+        it("can clone a date", function () {
+            var d = Date.now();
+            expect(utils.clone(d)).toBe(d);
+        });
+
+        it("can clone an object", function () {
+
+            var orig = {
+                a: {
+                    b: {
+                        c: "d"
+                    },
+                },
+                e: "f",
+                g: "unit"
+            },
+            expected = {
+                a: {
+                    b: {
+                        c: "d"
+                    },
+                },
+                e: "f",
+                g: "unit"
+            };
+
+            expect(utils.clone(orig)).toEqual(expected);
         });
     });
 });
