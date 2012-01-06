bee439c9c239b46a8de77fc891c9917793c2cd0a
Gord Tanner <gtanner@gmail.com>
2012-01-06 14:31:20 -0500

    work to get device ready firing for blackberry, still not really working

--- build/packager.js
+++ build/packager.js
@@ -13,28 +13,48 @@ module.exports = {
     modules: function (platform) {
         var files = [
                 "lib/utils.js",
+                "lib/channel.js",
                 "lib/plugin/navigator.js",
+                "lib/plugin/network.js",
                 "lib/plugin/notification.js",
                 "lib/plugin/accelerometer.js",
                 "lib/plugin/Connection.js",
-                "lib/plugin/network.js",
                 "lib/plugin/" + platform + "/device.js",
-                "lib/channel.js",
-                "lib/builder.js",
-                "lib/exec/" +platform + ".js"
+                "lib/builder.js"
             ]
             output = "";
 
-        //include modules
-        output += include(files, function (file, path) {
-            var id = path.replace(/lib\//, "phonegap/").replace(/\.js$/, ''); 
-            return "define('" + id + "', function (require, exports, module) {\n" + file + "});\n";
+        //HACK: this seem suspect to include like this
+        if (platform === "blackberry") {
+            files.unshift("lib/plugin/blackberry/PluginManager.js");
+            files.unshift("lib/plugin/blackberry/WebWorksPluginManager.js");
+        }
+
+        //include exec
+        output += include('lib/exec/' + platform + '.js', function (file, path) {
+            return "define('phonegap/exec'" +
+                   ", function (require, exports, module) {\n" + file + "});\n";
         });
 
         //include phonegap
         output += include('lib/phonegap.js', function (file, path) {
             return "define('phonegap'" +
                    ", function (require, exports, module) {\n" + file + "});\n";
+        });
+
+        //include platform
+        output += include('lib/platform/' + platform + '.js', function (file, path) {
+            return "define('phonegap/platform'" +
+                   ", function (require, exports, module) {\n" + file + "});\n";
+        });
+
+        //HACK: Get this in soon so we have access to it for the native layer
+        output += "window.PhoneGap = require('phonegap');";
+
+        //include modules
+        output += include(files, function (file, path) {
+            var id = path.replace(/lib\//, "phonegap/").replace(/\.js$/, ''); 
+            return "define('" + id + "', function (require, exports, module) {\n" + file + "});\n";
         });
 
         return output;
@@ -50,6 +70,7 @@ module.exports = {
 
         //include require
         output += include("thirdparty/almond.js");
+        output += "require.unordered = true;";
 
         //include modules
         output += this.modules(platform);
