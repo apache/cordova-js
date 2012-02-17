287a2b855455560320be0f96b90c9e22bba4ca45
Gord Tanner <gtanner@gmail.com>
2012-02-17 11:16:37 -0500

    added rest of contact tests and fixed bug in Contact.clone

--- test/test.contact.js
+++ test/test.contact.js
@@ -2,6 +2,95 @@ describe("Contact", function () {
     var Contact = require('cordova/plugin/Contact'),
         exec = require('cordova/exec');
 
+
+    describe("ctor", function () {
+        it("sets the default values", function () {
+            var c = new Contact();
+
+            expect(c.id).toBe(null);
+            expect(c.rawId).toBe(null);
+            expect(c.displayName).toBe(null);
+            expect(c.name).toBe(null);
+            expect(c.nickname).toBe(null);
+            expect(c.phoneNumbers).toEqual([]);
+            expect(c.emails).toEqual([]);
+            expect(c.addresses).toEqual([]);
+            expect(c.ims).toEqual([]);
+            expect(c.organizations).toEqual([]);
+            expect(c.birthday).toBe(null);
+            expect(c.note).toBe(null);
+            expect(c.photos).toEqual([]);
+            expect(c.categories).toEqual([]);
+            expect(c.urls).toEqual([]);
+        });
+
+        it("overrides default values with the arguments", function () {
+            var c = new Contact(
+                1,
+                "Bart",
+                "Bartholomew JoJo Simpson",
+                "Bart",
+                ["KL5-3223"],
+                ["bart@but.com"],
+                ["742 Evergreen Terrace, Springfield USA"],
+                ["1234567"],
+                ["Springfield Elementary"],
+                new Date("April 1, 1979"),
+                "Everybody in the house do the Bartman",
+                ["http://upload.wikimedia.org/wikipedia/en/thumb/e/ed/Bart_Simpson.svg/200px-Bart_Simpson.svg.png"],
+                ["simpson", "child"],
+                ["www.thesimpsons.com"]
+            );
+
+            expect(c.id).toBe(1);
+            expect(c.rawId).toBe(null);
+            expect(c.displayName).toBe("Bart");
+            expect(c.name).toBe("Bartholomew JoJo Simpson");
+            expect(c.nickname).toBe("Bart");
+            expect(c.phoneNumbers).toEqual(["KL5-3223"]);
+            expect(c.emails).toEqual(["bart@but.com"]);
+            expect(c.addresses).toEqual(["742 Evergreen Terrace, Springfield USA"]);
+            expect(c.ims).toEqual(["1234567"]);
+            expect(c.organizations).toEqual(["Springfield Elementary"]);
+            expect(c.birthday).toEqual(new Date("April 1, 1979")),
+            expect(c.note).toBe("Everybody in the house do the Bartman");
+            expect(c.photos).toEqual(["http://upload.wikimedia.org/wikipedia/en/thumb/e/ed/Bart_Simpson.svg/200px-Bart_Simpson.svg.png"]);
+            expect(c.categories).toEqual(["simpson", "child"]);
+            expect(c.urls).toEqual(["www.thesimpsons.com"]);
+        });
+    });
+
+    describe("clone", function () {
+        it("clears the id's on the cloned contact", function () {
+
+            var c = new Contact();
+            c.id = 1;
+            c.rawId = 1;
+
+            c.phoneNumbers.push({id: 1});
+            c.emails.push({id: 1});
+            c.addresses.push({id: 1});
+            c.ims.push({id: 1});
+            c.organizations.push({id: 1});
+            c.categories.push({id: 1});
+            c.photos.push({id: 1});
+            c.urls.push({id: 1});
+
+            var clone = c.clone();
+
+            expect(clone.id).toBeNull();
+            expect(clone.rawId).toBeNull();
+            expect(clone.phoneNumbers[0].id).toBeNull();
+            expect(clone.emails[0].id).toBeNull();
+            expect(clone.addresses[0].id).toBeNull();
+            expect(clone.ims[0].id).toBeNull();
+            expect(clone.organizations[0].id).toBeNull();
+            expect(clone.categories[0].id).toBeNull();
+            expect(clone.photos[0].id).toBeNull();
+            expect(clone.urls[0].id).toBeNull();
+        });
+    });
+
     describe("save", function () {
         it("calls exec when saving", function () {
             var c = new Contact(),
