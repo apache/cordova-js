94d7bb55b09c6ea3364abe92965cc9d03b3940ad
Fil Maj <maj.fil@gmail.com>
2012-02-28 17:01:56 -0800

    cleaning up a bug with registering/unregistering handlers in channel. added more tests for that. switched over use of handlers.length to new numHandlers variable in android and blackberry platform defs.

--- test/test.channel.js
+++ test/test.channel.js
@@ -1,3 +1,25 @@
 describe("channel", function () {
     var channel = require('cordova/channel');
+
+    describe("when subscribing", function() {
+    });
+
+    describe("when unsubscribing", function() {
+        it("should change the handlers length appropriately", function() {
+            var c = channel.create('test');
+            var firstHandler = function() {};
+            var secondHandler = function() {};
+            var thirdHandler = function() {};
+
+            c.subscribe(firstHandler);
+            c.subscribe(secondHandler);
+            c.subscribe(thirdHandler);
+
+            var initialLength = c.numHandlers;
+
+            c.unsubscribe(thirdHandler);
+
+            expect(c.numHandlers).toEqual(initialLength - 1);
+        });
+    });
 });
