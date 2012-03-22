95d979d69e306517c5487f431b0140da0886806f
Fil Maj <maj.fil@gmail.com>
2012-03-22 15:26:08 -0700

    start of geolocation rewrite to conform with Geolocation W3C API spec.

--- test/test.geolocation.js
+++ test/test.geolocation.js
@@ -1,32 +1,54 @@
 describe("geolocation", function () {
     var geo = require('cordova/plugin/geolocation'),
+        Position = require('cordova/plugin/Position'),
+        PositionError = require('cordova/plugin/PositionError'),
         s, e;
         exec = require('cordova/exec');
 
     beforeEach(function () {
         s = jasmine.createSpy("success");
         e = jasmine.createSpy("error");
+        exec.reset();
     });
 
     describe("when getting the current position", function () {
-        it("uses the default values", function () {
+        beforeEach(function() {
+            geo.lastPosition = null; // reset the cached position
+        });
+
+        it("uses default PositionOptions if none are specified", function () {
             geo.getCurrentPosition(s, e);
-            expect(exec).toHaveBeenCalledWith(jasmine.any(Function), jasmine.any(Function), "Geolocation", "getLocation", [false, 10000, 10000]);
+            expect(exec).toHaveBeenCalledWith(jasmine.any(Function), jasmine.any(Function), "Geolocation", "getLocation", [false, Infinity, 0]);
         });
 
-        it("uses the maximumAge option", function () {
+        it("uses the maximumAge option if specified", function () {
             geo.getCurrentPosition(s, e, {maximumAge: 10});
-            expect(exec).toHaveBeenCalledWith(jasmine.any(Function), jasmine.any(Function), "Geolocation", "getLocation", [false, 10000, 10]);
+            expect(exec).toHaveBeenCalledWith(jasmine.any(Function), jasmine.any(Function), "Geolocation", "getLocation", [false, Infinity, 10]);
         });
 
-        it("uses the enableHighAccuracy option", function () {
+        it("uses the enableHighAccuracy option if specified", function () {
             geo.getCurrentPosition(s, e, {enableHighAccuracy: true, maximumAge: 100});
-            expect(exec).toHaveBeenCalledWith(jasmine.any(Function), jasmine.any(Function), "Geolocation", "getLocation", [true, 10000, 100]);
+            expect(exec).toHaveBeenCalledWith(jasmine.any(Function), jasmine.any(Function), "Geolocation", "getLocation", [true, Infinity, 100]);
         });
 
-        it("uses the timeout option", function () {
+        it("uses the timeout option if specified and positive", function () {
             geo.getCurrentPosition(s, e, {timeout: 1000});
-            expect(exec).toHaveBeenCalledWith(jasmine.any(Function), jasmine.any(Function), "Geolocation", "getLocation", [false, 1000, 10000]);
+            expect(exec).toHaveBeenCalledWith(jasmine.any(Function), jasmine.any(Function), "Geolocation", "getLocation", [false, 1000, 0]);
+        });
+        it("uses a timeout value of 0 if specified and negative, which should call the error callback immediately (since we have no cached position)", function () {
+            geo.getCurrentPosition(s, e, {timeout: -1000});
+            expect(e).toHaveBeenCalledWith({
+                code:PositionError.TIMEOUT,
+                message:"timeout value in PositionOptions set to 0 and no cached Position object available, or cached Position object's age exceed's provided PositionOptions' maximumAge parameter."
+            });
+        });
+        it("can be used with one, two or three arguments", function() {
+            expect(function() { geo.getCurrentPosition(s); }).not.toThrow();
+            expect(function() { geo.getCurrentPosition(s,e); }).not.toThrow();
+            expect(function() { geo.getCurrentPosition(s,e,{}); }).not.toThrow();
+        });
+        it("should throw an exception if used with no arguments", function() {
+            expect(function() { geo.getCurrentPosition();}).toThrow("getCurrentPosition must be called with at least one argument.");
         });
     });
 
@@ -46,7 +68,7 @@ describe("geolocation", function () {
 
         it("sets an interval for the default timeout", function () {
             geo.watchPosition(s, e);
-            expect(window.setInterval).toHaveBeenCalledWith(jasmine.any(Function), 10000);
+            expect(window.setInterval).toHaveBeenCalledWith(jasmine.any(Function), Infinity);
         });
 
         it("sets an interval for the provided timeout", function () {
