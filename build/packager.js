2da5b44e4a603cf699279d7ef6b8d5d1b07b17ad
Patrick Mueller <pmuellr@apache.org>
2012-05-04 07:30:28 -0400

    [CB-634] need an sprintf implementation in utils
    * added utils.format() to utils.js
    * added test cases for utils.format() in test.utils.js
    * added some additional safety checks in the packager
      for the debug bundles
    * drive-by adding --verbose to .wr file
    
    Note that I also "refactored the source" of the
    utils.js module, which I'll be discussing on the
    m/l.

--- build/packager.js
+++ build/packager.js
@@ -166,6 +166,13 @@ function writeContents(oFile, fileName, contents, debug) {
         contents += '\n//@ sourceURL=' + fileName
         
         contents = 'eval(' + JSON.stringify(contents) + ')'
+        
+        // this bit makes it easier to identify modules
+        // with syntax errors in them
+        var handler = 'console.log("exception: in ' + fileName + ': " + e);'
+        handler += 'console.log(e.stack);'
+        
+        contents = 'try {' + contents + '} catch(e) {' + handler + '}'
     }
     
     else {
