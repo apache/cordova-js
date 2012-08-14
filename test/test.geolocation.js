f9b2fbc20b4d414a906a0e8287ed3bb2624921a4
Josh Soref <jsoref@rim.com>
2012-08-14 17:34:07 -0400

    Grammar: exceeds

--- test/test.geolocation.js
+++ test/test.geolocation.js
@@ -33,7 +33,7 @@ describe("geolocation", function () {
                 geo.getCurrentPosition(s, e, {timeout: -1000});
                 expect(e).toHaveBeenCalledWith({
                     code:PositionError.TIMEOUT,
-                    message:"timeout value in PositionOptions set to 0 and no cached Position object available, or cached Position object's age exceed's provided PositionOptions' maximumAge parameter."
+                    message:"timeout value in PositionOptions set to 0 and no cached Position object available, or cached Position object's age exceeds provided PositionOptions' maximumAge parameter."
                 });
             });
 
@@ -165,7 +165,7 @@ describe("geolocation", function () {
                 geo.watchPosition(s, e, {timeout: -1000});
                 expect(e).toHaveBeenCalledWith({
                     code:PositionError.TIMEOUT,
-                    message:"timeout value in PositionOptions set to 0 and no cached Position object available, or cached Position object's age exceed's provided PositionOptions' maximumAge parameter."
+                    message:"timeout value in PositionOptions set to 0 and no cached Position object available, or cached Position object's age exceeds provided PositionOptions' maximumAge parameter."
                 });
             });
 
