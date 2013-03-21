44fd67e6cc2f60e69aebda6b111fdbef00421160
Bryan Higgins <bhiggins@blackberry.com>
2013-03-21 13:52:57 -0400

    [CB-2676] Add prompt to Notification API for BlackBerry10

--- test/blackberry/webworks/test.notification.js
+++ test/blackberry/webworks/test.notification.js
@@ -21,15 +21,16 @@
 
 describe("notification", function () {
     var notification = require('cordova/plugin/webworks/notification'),
-        cordova = require('cordova');
+        cordova = require('cordova'),
+        platform = require('cordova/platform');
 
     beforeEach(function(){
         global.blackberry = {
             ui:{
                 dialog:{
-                    customAskAsync: jasmine.createSpy()
-                        // apply: jasmine.createSpy('apply')
-                    
+                    customAskAsync: jasmine.createSpy(),
+                    standardAskAsync: jasmine.createSpy(),
+                    D_PROMPT: jasmine.createSpy(),
                 }
             }
         }
@@ -82,6 +83,74 @@ describe("notification", function () {
                 status: cordova.callbackStatus.NO_RESULT,
                 message: "WebWorks Is On It"
             });
+        });
+    });
+
+    describe("prompt", function() {
+
+        var runtime;
+
+        beforeEach(function () {
+            runtime = "qnx";
+            spyOn(platform, "runtime").andCallFake(function () {
+                return runtime;
+            });
+        });
+
+        it("should return that prompt arguments are missing", function () {
+            expect(notification.prompt("")).toEqual({
+                status: 9,
+                message: "Notification action - prompt arguments not found"
+            });
+        });
+
+        it("should return that WebWorks Is On It", function () {
+            expect(notification.prompt(["Message", "Title", []], function () {})).toEqual({
+                status: cordova.callbackStatus.NO_RESULT,
+                message: "WebWorks Is On It"
+            });
+        });
+
+        it("should return that runtime is not supported", function () {
+            runtime = "java";
+            expect(notification.prompt(["Message", "Title", []], function () {})).toEqual({
+                status: 9,
+                message: "Notification action - runtime not supported"
+            });
+        });
+
+        it("should call the blackberry object", function() {
+            notification.prompt(["Message", "Title", []], function () {});
+
+            expect(blackberry.ui.dialog.standardAskAsync).toHaveBeenCalledWith(
+                "Message", 
+                blackberry.ui.dialog.D_PROMPT, 
+                jasmine.any(Function), 
+                { "title" : "Title" });
+        });
+
+        it("should invoke callback with result object for Ok", function() {
+            var win = jasmine.createSpy('win');
+
+            blackberry.ui.dialog.standardAskAsync.andCallFake(function (a, b, callback) {
+                callback({"return": "Ok", promptText: "Hello"});
+            });
+
+            notification.prompt(["Message", "Title", []], win);
+
+            expect(win).toHaveBeenCalledWith({buttonIndex: 0 , input1: "Hello"});
+        });
+
+        it("should invoke callback with result object for Cancel", function() {
+            var win = jasmine.createSpy('win');
+
+            blackberry.ui.dialog.standardAskAsync.andCallFake(function (a, b, callback) {
+                callback({"return": "Cancel"});
+            });
+
+            notification.prompt(["Message", "Title", []], win);
+
+            expect(win).toHaveBeenCalledWith({buttonIndex: 1});
         });
     });
 
