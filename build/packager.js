e13fbe7a44837031e70518b49ca1ddb6ecbd3a13
Jesse MacFadyen <purplecabbage@gmail.com>
2013-07-11 15:59:31 -0700

    catch exception for missing or invalid file path (cherry picked from commit 87cc336cb59f8df559a201fdb1e2a6251b57a06b)

--- build/packager.js
+++ build/packager.js
@@ -188,28 +188,33 @@ function collectFiles(dir, id) {
 
     var result = {}    
     
-    var entries = fs.readdirSync(dir)
-    
-    entries = entries.filter(function(entry) {
-        if (entry.match(/\.js$/)) return true
-        
-        var stat = fs.statSync(path.join(dir, entry))
-        if (stat.isDirectory())  return true
-    })
+    try {
+        var entries = fs.readdirSync(dir)
 
-    entries.forEach(function(entry) {
-        var moduleId = path.join(id, entry)
-        var fileName = path.join(dir, entry)
-        
-        var stat = fs.statSync(fileName)
-        if (stat.isDirectory()) {
-            copyProps(result, collectFiles(fileName, moduleId))
-        }
-        else {
-            moduleId         = getModuleId(moduleId)
-            result[moduleId] = fileName
-        }
-    })
+        entries = entries.filter(function(entry) {
+            if (entry.match(/\.js$/)) return true
+            
+            var stat = fs.statSync(path.join(dir, entry))
+            if (stat.isDirectory())  return true
+        })
+
+        entries.forEach(function(entry) {
+            var moduleId = path.join(id, entry)
+            var fileName = path.join(dir, entry)
+            
+            var stat = fs.statSync(fileName)
+            if (stat.isDirectory()) {
+                copyProps(result, collectFiles(fileName, moduleId))
+            }
+            else {
+                moduleId         = getModuleId(moduleId)
+                result[moduleId] = fileName
+            }
+        })
+    }
+    catch(ex) {
+
+    }
     
     return copyProps({}, result)
 }
