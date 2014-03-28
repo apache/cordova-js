a64a94cf4217dbf7ec588fb6674a55ca2aa321fd
Edward Grech <edward.grech@uniblue.com>
2014-03-28 15:11:01 +0100

    Fixed "WARNING: file name src\ubuntu\platform.js is missing the license header" during compile
    github: close #67

--- src/ubuntu/platform.js
+++ src/ubuntu/platform.js
@@ -1,4 +1,5 @@
 /*
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -17,6 +18,7 @@
  * under the License.
  *
 */
+
 module.exports = {
     id: "ubuntu",
     bootstrap: function() {
