c893a47254111e26d8b11f28972793491d83c4cf
Jesse MacFadyen <purplecabbage@gmail.com>
2014-04-28 14:46:59 -0700

    CB-6539 Future proof the WinJS include

--- src/windows8/platform.js
+++ src/windows8/platform.js
@@ -47,9 +47,16 @@ module.exports = {
         };
 
         if (!window.WinJS) {
-            // <script src="//Microsoft.WinJS.1.0/js/base.js"></script>
+            // <script src="//Microsoft.WinJS.1.0/js/base.js"></script> 
             var scriptElem = document.createElement("script");
-            scriptElem.src = "//Microsoft.WinJS.1.0/js/base.js";
+            if (navigator.appVersion.indexOf("MSAppHost/2.0;") > -1) { 
+                // windows 8.1 + IE 11
+                scriptElem.src = "//Microsoft.WinJS.2.0/js/base.js";
+            }
+            else {  
+                // windows 8.0 + IE 10
+                scriptElem.src = "//Microsoft.WinJS.1.0/js/base.js"; 
+            }
             scriptElem.addEventListener("load", onWinJSReady);
             document.head.appendChild(scriptElem);
         }
