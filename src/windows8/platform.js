a955ca7afb505a9368870190cfcbed16001d32f6
Andrew Grieve <agrieve@chromium.org>
2014-04-29 00:09:03 -0400

    Fix cordova-js grunt failing due to jshint errors from WinJS commit

--- src/windows8/platform.js
+++ src/windows8/platform.js
@@ -47,15 +47,15 @@ module.exports = {
         };
 
         if (!window.WinJS) {
-            // <script src="//Microsoft.WinJS.1.0/js/base.js"></script> 
+            // <script src="//Microsoft.WinJS.1.0/js/base.js"></script>
             var scriptElem = document.createElement("script");
-            if (navigator.appVersion.indexOf("MSAppHost/2.0;") > -1) { 
+            if (navigator.appVersion.indexOf("MSAppHost/2.0;") > -1) {
                 // windows 8.1 + IE 11
                 scriptElem.src = "//Microsoft.WinJS.2.0/js/base.js";
             }
-            else {  
+            else {
                 // windows 8.0 + IE 10
-                scriptElem.src = "//Microsoft.WinJS.1.0/js/base.js"; 
+                scriptElem.src = "//Microsoft.WinJS.1.0/js/base.js";
             }
             scriptElem.addEventListener("load", onWinJSReady);
             document.head.appendChild(scriptElem);
