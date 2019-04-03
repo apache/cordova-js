cdfaf768d2da1ce84621b1bd4ea298d4e2557cc9
Raphael von der Grün <raphinesse@gmail.com>
2019-04-03 12:02:55 +0200

    Completely refactor build system
    The goals of this refactoring are the following:
    
    - Improve code quality of the build system
    - Prepare code base for adding code coverage
    - Prepare code base for exposing the build system as a package export,
      so platforms can build their `cordova.js` during their build (or
      creation) process. This would make the JS build using coho obsolete.
    - Prepare code base to remove dependency on Grunt
    
    The build process now lives under `build-tools` (was `tasks`). It does
    not depend on Grunt anymore but is written in plain Node.js.
    
    The original Grunt interface for building (as used by coho) was
    preserved and is implemented in `Gruntfile.js` using the new build
    system. The `platformName` option and support for getting platform paths
    from the `cordova-platforms` key in `package.json` have been removed
    from the Grunt interface, but neither of those are used in coho.
    
    The logic that is specific to the test build has been extracted from
    the rest of the build. It now lives in `test/build.js` and is run
    automatically during `npm test`.
    
    The following dependencies have been added:
    
    - execa
    - fs-extra
    - globby
    
    I have taken extra care to preserve the exact format of the built file.
    This means that the correctness of the refactoring can be verified by
    simply diffing the build artifacts in `pkg` created with and without
    this change.

---
+++ build-tools/bundle.js
@@ -0,0 +1,63 @@
+/*!
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+module.exports = function bundle (scripts, modules, config) {
+    const context = Object.assign({
+        modules: modules.map(m => m.contents).join('\n'),
+        includeScript: name => scripts[name].contents
+    }, config);
+
+    return bundleTemplate(context);
+};
+
+const bundleTemplate = ({
+    commitId,
+    platformName,
+    platformVersion,
+    includeScript,
+    modules
+}) => `
+// Platform: ${platformName}
+// ${commitId}
+/*
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements.  See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership.  The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+\x20
+     http://www.apache.org/licenses/LICENSE-2.0
+\x20
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied.  See the License for the
+ specific language governing permissions and limitations
+ under the License.
+*/
+;(function() {
+var PLATFORM_VERSION_BUILD_LABEL = '${platformVersion}';
+${includeScript('require')}
+${modules}
+window.cordova = require('cordova');
+${includeScript('bootstrap')}
+})();
+`.trim();
---
+++ build-tools/modules.js
@@ -0,0 +1,80 @@
+/*!
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+const path = require('path');
+
+const {
+    readContents,
+    stripLicenseHeader,
+    prependFileComment,
+    values,
+    pkgRoot,
+    collectModules
+} = require('./common');
+
+module.exports = function modules (config) {
+    for (const m of values(config.extraModules)) {
+        if (m.path.startsWith('../')) {
+            m.path = path.resolve(m.path);
+        }
+    }
+
+    const commonModules = collectCommonModules();
+    const modules = values(Object.assign(commonModules, config.extraModules));
+    modules.sort((a, b) => a.moduleId.localeCompare(b.moduleId));
+    return Promise.all(modules.map(modulePipeline(config)));
+};
+
+function collectCommonModules () {
+    const modules = collectModules(path.join(pkgRoot, 'src/common'));
+    modules[''] = {
+        moduleId: '',
+        path: path.relative(pkgRoot, path.join(pkgRoot, 'src/cordova.js'))
+    };
+    return modules;
+}
+
+function modulePipeline (config) {
+    return f => Promise.resolve(f)
+        .then(readContents)
+        .then(config.preprocess)
+        .then(stripLicenseHeader)
+        .then(addModuleNamespace('cordova'))
+        .then(wrapInModuleContext)
+        .then(prependFileComment);
+}
+
+function addModuleNamespace (ns) {
+    return m => {
+        const moduleId = path.posix.join(ns, m.moduleId);
+        return Object.assign({}, m, { moduleId });
+    };
+}
+
+function wrapInModuleContext (f) {
+    const contents = moduleTemplate({ id: f.moduleId, body: f.contents });
+    return Object.assign({}, f, { contents });
+}
+
+const moduleTemplate = ({ id, body }) => `
+define("${id}", function(require, exports, module) {
+
+${body}
+});
+`.trimLeft();
