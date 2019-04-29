8c3339e1dba66d81fa209e520b8a5a188ee80505
Raphael von der Grün <raphinesse@gmail.com>
2019-04-29 23:46:01 +0200

    Collect & report test coverage for our browser code

--- karma.conf.js
+++ karma.conf.js
@@ -48,7 +48,11 @@ module.exports = function (config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+        reporters: ['progress', 'coverage'],
+
+        coverageReporter: {
+            type: 'text'
+        },
 
         // web server port
         port: 9876,
